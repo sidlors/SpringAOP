6be45ba4e47b26db0f181c8361b5fc5a7c64ff97
sidlors <sidlors@gmail.com>
2015-09-26 23:59:20 -0500

    first commit

---
+++ src/main/java/mx/com/sidlors/aop/HijackThrowException.java
@@ -0,0 +1,9 @@
+package mx.com.sidlors.aop;
+
+import org.springframework.aop.ThrowsAdvice;
+
+public class HijackThrowException implements ThrowsAdvice {
+	public void afterThrowing(IllegalArgumentException e) throws Throwable {
+		System.out.println("HijackThrowException : Throw exception hijacked!");
+	}
+}
---
+++ src/main/java/mx/com/sidlors/common/App.java
@@ -0,0 +1,28 @@
+package mx.com.sidlors.common;
+
+import mx.com.sidlors.customer.services.CustomerService;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext(
+				new String[] { "Spring-Customer.xml" });
+
+		CustomerService cust = (CustomerService) appContext
+				.getBean("customerServiceProxy");
+
+		System.out.println("*************************");
+		cust.printName();
+		System.out.println("*************************");
+		cust.printURL();
+		System.out.println("*************************");
+		try {
+			cust.printThrowException();
+		} catch (Exception e) {
+
+		}
+
+	}
+}
